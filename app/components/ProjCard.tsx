7ead21e68661c300aed9779d987319ecfdec6cca
Keturah Howard <keturah.dev@gmail.com>
2024-05-15 23:12:31 -0700

    make basic grid and cards

--- app/components/ProjCard.tsx
+++ app/components/ProjCard.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from "react";
 
 type ProjCardProp = {
-  key: number,
-  title: string,
-  img: string,
-  description?: string
-}
+  key: number;
+  title: string;
+  img: string;
+  description?: string;
+};
 
-const ProjCard = async ({title, img, description}: ProjCardProp) => {
-console.log("thing:", img)
+const ProjCard = async ({ title, img, description }: ProjCardProp) => {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
-      <figure><img src={'img'} alt="Shoes" /></figure>
+      <figure>
+        <img src={img} alt="Shoes" />
+      </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         {description ? <p>{description}</p> : null}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjCard
+export default ProjCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
-import ProjCard from "./components/ProjCard"
+import next from "next";
+import ProjCard from "./components/ProjCard";
 import fakeData from "./util/project";
 
 export default function Home() {
   return (
     <main className="m-10 my-20 flex flex-col justify-center">
-        <h1 className="font-bold">Proj Board</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
-          {fakeData.map( async (p, i) => {
-            const res = await fetch(p.img, {});
-            const json = await res.json();
-            return <ProjCard key={i} title={p.title} img={json.errors ? "" : json.data}/>
-          })}
-        </div>
+      <h1 className="font-bold">Proj Board</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
+        {fakeData.map(async (p, i) => {
+          const res = await fetch(p.img, { cache: "no-store" });
+          const json = await res.json();
+          return (
+            <ProjCard
+              key={i}
+              title={p.title}
+              img={json.errors ? "" : json[0].url}
+            />
+          );
+        })}
+      </div>
     </main>
-  )
+  );
 }
